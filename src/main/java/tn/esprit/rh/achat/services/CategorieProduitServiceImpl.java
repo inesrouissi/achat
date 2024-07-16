dda7f7e00a19f40379b5deabbf37125f22719e1f
rouissi <ines.rouissi@esprit.tn>
2024-07-16 11:52:42 +0100

    initialisation du projet achat

--- src/main/java/tn/esprit/rh/achat/services/CategorieProduitServiceImpl.java
+++ src/main/java/tn/esprit/rh/achat/services/CategorieProduitServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Service
 public class CategorieProduitServiceImpl implements ICategorieProduitService {
 
-	
+	// ines
 	@Autowired
 	CategorieProduitRepository categorieProduitRepository;
 	@Override
